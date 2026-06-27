fd9c0d47a9e59707ad4190c534ef4d13fe9c8cec
unknown <nabasasimanungkalit@gmail.com>
2026-06-27 15:47:42 +0700

    insert complete data

--- gallery-data.js
+++ gallery-data.js
@@ -1,51 +1,126 @@
 // gallery-data.js - Photo library configuration catalog
 
 const fullGalleryData = [
+  { title: "Beach Landscape", type: "image", url: "images/BeachLandscape.jpg" },
+  { title: "Blue Hour", type: "image", url: "images/BlueHour.jpg" },
   {
-    title: "Cascading Waterfall",
+    title: "Slow Shutter Portraits",
+    type: "image",
+    url: "images/CircleSlowShutter.jpg",
+  },
+  { title: "Feast", type: "image", url: "images/Feast.jpg" },
+  { title: "Flower Power", type: "image", url: "images/FlowerPower.jpg" },
+  {
+    title: "Leaf Macro Water",
+    type: "image",
+    url: "images/LeafMacroWater.JPG",
+  },
+  { title: "Love", type: "image", url: "images/Love.JPG" },
+  { title: "Moon Eclipse", type: "image", url: "images/MoonEclipse.jpg" },
+  {
+    title: "Shanghai Park Slow Shutter",
+    type: "image",
+    url: "images/ShanghaiParkSlowShutter.jpg",
+  },
+  { title: "Slow Waterfall", type: "image", url: "images/SlowWaterfall.JPG" },
+  {
+    title: "Bird Over The Water",
+    type: "image",
+    url: "images/BirdOverTheWater.jpg",
+  },
+  { title: "Cathway Pacific", type: "image", url: "images/CathwayPacific.jpg" },
+  {
+    title: "Concert With Sunset",
+    type: "image",
+    url: "images/ConcertWithSunset.jpg",
+  },
+  { title: "Felix Pose", type: "image", url: "images/FelixPose.jpg" },
+  {
+    title: "Framed Waterfall",
     type: "image",
     url: "images/FramedWaterfall.jpg",
   },
   {
-    title: "Slowed tower",
+    title: "Lemon Sharp Look",
     type: "image",
-    url: "images/ShanghaiParkSlowShutter.jpg",
+    url: "images/LemonSharpLook.JPG",
   },
   {
-    title: "Coastal Landscape",
-    type: "image",
-    url: "images/BeachLandscape.jpg",
-  },
-  {
-    title: "Colorful Reptile",
-    type: "image",
-    url: "images/LizardOnTheRock.jpg",
-  },
-  {
-    title: "Golfing with a View",
-    type: "image",
-    url: "images/GolfView.jpg",
-  },
-  {
-    title: "Feeding the Birds",
+    title: "Low Angle Feed Birds",
     type: "image",
     url: "images/LowAngleFeedBirds.jpg",
   },
   {
-    title: "Architectural Garden",
+    title: "Plane Silhouette",
+    type: "image",
+    url: "images/PlaneSihloette.jpg",
+  },
+  { title: "Shining Cactus", type: "image", url: "images/ShinngCactus.JPG" },
+  {
+    title: "Touching The Pink Sky",
+    type: "image",
+    url: "images/TouchingThePinkSky.jpg",
+  },
+  {
+    title: "Bird Walking On The Water",
+    type: "image",
+    url: "images/BirdWalkingOntheWater.jpg",
+  },
+  { title: "Cats Love", type: "image", url: "images/CatsLove.JPG" },
+  {
+    title: "Dreamy Architecture",
     type: "image",
     url: "images/DreamyArchitecture.jpg",
   },
+  { title: "Feo Smile", type: "image", url: "images/FeoSmile.JPG" },
+  { title: "Golf View", type: "image", url: "images/GolfView.jpg" },
+  { title: "Light Painting", type: "image", url: "images/LightPainting.jpg" },
+  { title: "Macro Leaf", type: "image", url: "images/MacroLeaf.jpg" },
   {
-    title: "Moon Eclipse",
-    type: "image",
-    url: "images/MoonEclipse.jpg",
-  },
-  {
-    title: "Lights. Camera. CATS",
+    title: "Playing With Lights",
     type: "image",
     url: "images/PlayingWithLights.jpg",
   },
+  {
+    title: "Sightseeing Birds",
+    type: "image",
+    url: "images/SightseeingBirds.jpg",
+  },
+  {
+    title: "White Bird Smiling",
+    type: "image",
+    url: "images/WhiteBirdSmiling.jpg",
+  },
+  {
+    title: "Black and White Padel",
+    type: "image",
+    url: "images/BlackandWhitePadel.jpg",
+  },
+  {
+    title: "Christmas Festive",
+    type: "image",
+    url: "images/ChristmasFestive.jpg",
+  },
+  {
+    title: "End of Chinese Building",
+    type: "image",
+    url: "images/EndofChineseBuilding.jpg",
+  },
+  { title: "Flower Macro", type: "image", url: "images/FlowerMacro.jpg" },
+  { title: "Kakaktua Wings", type: "image", url: "images/KakakTuaWings.jpg" },
+  {
+    title: "Lizard On The Rock",
+    type: "image",
+    url: "images/LizardOnTheRock.jpg",
+  },
+  { title: "Macro Water", type: "image", url: "images/MacroWater.jpg" },
+  { title: "River Slow", type: "image", url: "images/RiverSlow.JPG" },
+  {
+    title: "Simple Architecture",
+    type: "image",
+    url: "images/SimpleArchitecture.jpg",
+  },
+  { title: "Wide Feo Shot", type: "image", url: "images/WideFeoShot.RAF" },
 ];
 
 if (typeof module !== "undefined" && module.exports) {
